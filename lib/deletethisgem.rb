f7a6a5d05a142b6c0d8211afd2a92ffa8ced40d0
Sean Kelly <shonkelli@gmail.com>
2017-04-08 20:14:26 -0400

    added word.rb

--- lib/deletethisgem.rb
+++ lib/deletethisgem.rb
@@ -1,5 +1,5 @@
 require "deletethisgem/version"
-
+require "deletethisgem/word"
 module Deletethisgem
   # Your code goes here...
 end
---
+++ lib/deletethisgem/word.rb
@@ -0,0 +1,9 @@
+# lib/streverser/word.rb
+# require_relative 'some_other_file'
+module Deletethisgem
+class Word
+  def self.reversify (input)
+    input.reverse
+  end
+end
+end
